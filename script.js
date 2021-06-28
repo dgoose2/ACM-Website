1e2d43c4c43e04fd8d3b4c6cf9ce65b85281b080
Andrew Cantrell <andyroocantrell@gmail.com>
2021-06-27 18:34:52 -0700

    Changed a few things - A

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function myPage() {
     window.location = ("about/index.html");
 }
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
